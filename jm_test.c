b9673a6adcb01f0c09cd51407bb47194de9a19df
LinDol <lindolsang@LinDolui-MacBook-Pro.local>
2018-11-26 00:51:48 +0900

    Implement set operation

--- jm_test.c
+++ jm_test.c
@@ -29,6 +29,8 @@
     } while (0); })
 
 const char* jm_object_get_string_member(JsonObject* obj, int length, ...);
+gboolean jm_object_set_string_member(JsonObject* obj, const char*, int length, ...);
+
 int cut_array_index(const char* index_str) {
     char buf[10] = {0, };
     snprintf(buf, 10, "%s", index_str + 1);
@@ -66,7 +68,7 @@ const char* jm_object_get_string_member(JsonObject* obj, int length, ...)
                     if (json_object_has_member(c_obj, member_str)) {
                         member_node = json_object_get_member(c_obj, member_str);
                     } else {
-                        ERR("else!\n");
+                        ERR("Invalid member: %s\n", member_str);
                     }
                 }
                 break;
@@ -100,6 +102,61 @@ const char* jm_object_get_string_member(JsonObject* obj, int length, ...)
     va_end(valist);
 
     return (i == length) ? return_value : NULL;
+}
+
+gboolean jm_object_set_string_member(JsonObject* obj, const char* value, int length, ...)
+{
+    va_list valist;
+    int i = 0;
+    gboolean is_setted = FALSE;
+
+    va_start(valist, length);
+
+    JsonObject* c_obj = obj;
+    char* member_str = va_arg(valist, char*);
+
+    if (json_object_has_member(c_obj, member_str) == FALSE) {
+        ERR("Invalid member: %s\n", member_str);
+        return is_setted;
+    }
+
+    JsonNode* member_node = json_object_get_member(c_obj, member_str);
+
+    for (; i < length; i++) {
+        JsonNodeType node_type = json_node_get_node_type(member_node);
+        switch (node_type) {
+            case JSON_NODE_OBJECT:
+                DBG("JSON_NODE_OBJECT\n");
+                {
+                    c_obj = json_node_get_object(member_node);
+                    member_str = va_arg(valist, char*);
+                    if (json_object_has_member(c_obj, member_str)) {
+                        member_node = json_object_get_member(c_obj, member_str);
+                    } else {
+                        ERR("Invalid member: %s\n", member_str);
+                    }
+                }
+                break;
+            case JSON_NODE_ARRAY:
+                DBG("JSON_NODE_ARRAY\n");
+                break;
+            case JSON_NODE_VALUE:
+                DBG("JSON_NODE_VALUE\n");
+                {
+                    json_node_set_string(member_node, value);
+                }
+                break;
+            case JSON_NODE_NULL:
+                DBG("JSON_NODE_NULL\n");
+                break;
+            default:
+                WRN("Unsupported type\n");
+        }
+    }
+
+    va_end(valist);
+
+    return is_setted;
 }
 
 int test_get(const char* file_name)
@@ -136,6 +193,55 @@ int test_get(const char* file_name)
     return EXIT_SUCCESS;
 }
 
+int json_save_file(JsonNode* root, const char* file_name)
+{
+    JsonGenerator *gen = json_generator_new();
+    json_generator_set_root(gen, root);
+    GError *error = NULL;
+    json_generator_to_file(gen, file_name, &error);
+    if (error) {
+        g_print("Unable to save `%s`: %s\n", file_name, error->message);
+        g_error_free(error);
+        g_object_unref(gen);
+        return EXIT_FAILURE;
+    }
+    g_object_unref(gen);
+    return EXIT_SUCCESS;
+}
+
+int test_set(const char* file_name)
+{
+    JsonParser *parser = NULL;
+    JsonNode *root = NULL;
+    GError *error = NULL;
+
+    parser = json_parser_new();
+    error = NULL;
+    json_parser_load_from_file(parser, file_name, &error);
+    if (error) {
+        g_print("Unable to parse `%s`: %s\n", file_name, error->message);
+        g_error_free(error);
+        g_object_unref(parser);
+        return EXIT_FAILURE;
+    }
+
+    root = json_parser_get_root(parser);
+    /* manipulate the object tree and then exit */
+    JsonObject* obj = json_node_get_object(root);
+    gboolean is_set = jm_object_set_string_member(obj, "Test Title", 3, "widget", "window", "title");
+    if (is_set) {
+        DBG("seted in 'widget.window.title'\n");
+    } else {
+        DBG("not seted in 'widget.window.title`\n");
+    }
+
+    /* save output */
+    json_save_file(root, "sample_out.json");
+
+    g_object_unref(parser);
+    return EXIT_SUCCESS;
+}
+
 int main(int argc, char* argv[])
 {
 
@@ -145,6 +251,7 @@ int main(int argc, char* argv[])
     }
 
     test_get(argv[1]);
+    test_set(argv[1]);
 
     return EXIT_SUCCESS;
 }
