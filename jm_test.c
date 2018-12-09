d8f98acb15582a2a429d094a3ae77244e5df38d1
Juyeon Park <lindolsang@nate.com>
2018-12-09 22:15:28 +0900

    Implement node clone

--- jm_test.c
+++ jm_test.c
@@ -358,6 +358,235 @@ int test_int_get(const char* file_name)
     return EXIT_SUCCESS;
 }
 
+/**
+  @see https://developer.gnome.org/gobject/stable/gobject-Type-Information.html#GType
+  */
+static void __gtype_print(GType t)
+{
+    switch (t) {
+        case G_TYPE_INVALID:
+            DBG("G_TYPE_INVALID");
+            break;
+        case G_TYPE_NONE:
+            DBG("G_TYPE_NONE");
+            break;
+        case G_TYPE_INTERFACE:
+            DBG("G_TYPE_INTERFACE");
+            break;
+        case G_TYPE_CHAR:
+            DBG("G_TYPE_CHAR");
+            break;
+        case G_TYPE_UCHAR:
+            DBG("G_TYPE_UCHAR");
+            break;
+        case G_TYPE_BOOLEAN:
+            DBG("G_TYPE_BOOLEAN");
+            break;
+        case G_TYPE_INT:
+            DBG("G_TYPE_INT");
+            break;
+        case G_TYPE_UINT:
+            DBG("G_TYPE_UNIT");
+            break;
+        case G_TYPE_LONG:
+            DBG("G_TYPE_LONG");
+            break;
+        case G_TYPE_ULONG:
+            DBG("G_TYPE_ULONG");
+            break;
+        case G_TYPE_INT64:
+            DBG("G_TYPE_INT64");
+            break;
+        case G_TYPE_UINT64:
+            DBG("G_TYPE_UINT64");
+            break;
+        case G_TYPE_ENUM:
+            DBG("G_TYPE_ENUM");
+            break;
+        case G_TYPE_FLAGS:
+            DBG("G_TYPE_FLAGS");
+            break;
+        case G_TYPE_FLOAT:
+            DBG("G_TYPE_FLOAT");
+            break;
+        case G_TYPE_DOUBLE:
+            DBG("G_TYPE_DOUBLE");
+            break;
+        case G_TYPE_STRING:
+            DBG("G_TYPE_STRING");
+            break;
+        case G_TYPE_POINTER:
+            DBG("G_TYPE_POINTER");
+            break;
+        case G_TYPE_BOXED:
+            DBG("G_TYPE_BOXED");
+            break;
+        case G_TYPE_PARAM:
+            DBG("G_TYPE_PARAM");
+            break;
+        case G_TYPE_OBJECT:
+            DBG("G_TYPE_OBJECT");
+            break;
+        /*case G_TYPE_GTYPE:
+            DBG("G_TYPE_GTYPE");
+            break;*/
+        case G_TYPE_VARIANT:
+            DBG("G_TYPE_VARIANT");
+            break;
+        default:
+            DBG("others: %ld", t);
+            break;
+    }
+}
+
+/*
+   1. get node type
+          if is object
+              get members
+                  get member
+                      go to 1.
+          if is array
+              get elements
+                  get element
+                      go to 1.
+          if is value
+              get type
+                  if is int
+                      add int value
+                  if is double
+                      add double value
+                  if is boolean 
+                      add boolean value
+                  if is string
+                      add string value
+                  if is null
+                      add null value
+*/
+static int __jm_json_node_build(JsonNode* node, JsonBuilder* builder)
+{
+    JsonNodeType node_type = json_node_get_node_type(node);
+    switch (node_type) {
+        case JSON_NODE_OBJECT:
+            {
+                JsonObject* node_obj = json_node_get_object(node);
+                GList* obj_member_list = json_object_get_members(node_obj);
+                GList* l;
+
+                json_builder_begin_object(builder);
+
+                for (l = obj_member_list; l != NULL; l = l->next) {
+                    char* member_node_name = (char* )l->data;
+                    JsonNode* member_node = json_object_get_member(node_obj, member_node_name);
+                    json_builder_set_member_name(builder, member_node_name);
+
+                    __jm_json_node_build(member_node, builder);
+                }
+
+                json_builder_end_object(builder);
+                g_list_free(obj_member_list);
+            }
+            break;
+        case JSON_NODE_ARRAY:
+            {
+                JsonArray* node_arr = json_node_get_array(node);
+                GList* arr_member_list = json_array_get_elements(node_arr);
+                GList* l = NULL;
+
+                json_builder_begin_array(builder);
+
+                for (l = arr_member_list; l != NULL; l = l->next) {
+                    JsonNode* member_node = (JsonNode*)l->data;
+                    __jm_json_node_build(member_node, builder);
+                }
+
+                json_builder_end_array(builder);
+                /* what is the free data container? transfer container */
+                //TODO needs to check and confirm whether this block has memory leak or not
+                //https://developer.gnome.org/glib/stable/glib-Doubly-Linked-Lists.html
+                //https://developer.gnome.org/json-glib/stable/json-glib-JSON-Array.html#json-array-get-elements
+                g_list_free(arr_member_list);
+            }
+            break;
+        case JSON_NODE_VALUE:
+            {
+                GType mv_type = json_node_get_value_type(node);
+                switch (mv_type) {
+                    case G_TYPE_STRING:
+                        {
+                            json_builder_add_string_value(builder, json_node_get_string(node));
+                        }
+                        break;
+                    case G_TYPE_INT64:
+                        {
+                            json_builder_add_int_value(builder, json_node_get_int(node));
+                        }
+                        break;
+                    default:
+                        WRN("Unsupported type - %ld", mv_type);
+                        // to debug
+                        __gtype_print(mv_type);
+                        break;
+                }
+            }
+            break;
+        case JSON_NODE_NULL:
+            {
+                WRN("JSON_NODE_NULL");
+            }
+            break;
+    }
+    return 0;
+}
+
+/**
+ This api will make the new JsonNode like clone
+ Even if @node has immutable property, returned value will not have immutable.
+
+ @param node the pointer that is used for source
+ @return the pointer of JsonNode that has cloned
+ @remark You should release returned value using json_node_unref()
+  */
+JsonNode* jm_node_clone(JsonNode* node)
+{
+    JsonBuilder* builder = json_builder_new();
+    __jm_json_node_build(node, builder);
+    JsonNode* return_node = json_builder_get_root(builder);
+    g_object_unref(builder);
+
+    return return_node;
+}
+
+int test_clone(const char* file_name)
+{
+    JsonParser *parser;
+    JsonNode *root;
+    GError *error;
+
+    parser = json_parser_new();
+    error = NULL;
+    json_parser_load_from_file(parser, file_name, &error);
+    if (error) {
+        ERR("Unable to parse `%s': %s", file_name, error->message);
+        g_error_free(error);
+        g_object_unref(parser);
+        return EXIT_FAILURE;
+    }
+
+    root = json_parser_get_root(parser);
+
+    /* manipulate the object tree and then exit */
+    JsonNode* root_clone = jm_node_clone(root); 
+    
+    g_object_unref(parser);
+
+    // test output
+    json_save_file(root_clone, "sample_clone.json");
+
+    json_node_unref(root_clone);
+
+    return EXIT_SUCCESS;
+}
+
 int main(int argc, char* argv[])
 {
 
@@ -366,9 +595,10 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
-    test_get(argv[1]);
-    test_set(argv[1]);
-    test_int_get(argv[1]);
+    //test_get(argv[1]);
+    //test_set(argv[1]);
+    //test_int_get(argv[1]);
+    test_clone(argv[1]);
 
     return EXIT_SUCCESS;
 }
